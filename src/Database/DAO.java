6a1457297f735e968903a2f1ab4b4f9ad64c2f77
SunggyuLee <sq0216@gmail.com>
2018-12-05 22:49:39 +0900

    Database_settings

---
+++ src/Database/DAO.java
@@ -0,0 +1,154 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAO {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/db_term_1?autoReconnect=true&useSSL=false";
+	static final String USERNAME = "root";
+	static final String PASSWORD = "dpahd396";
+
+	static {
+		try {
+			Class.forName(JDBC_DRIVER);
+		} catch(ClassNotFoundException e) {
+			System.out.println("클래스 로드 실패 : "+e.getMessage());
+		}
+	}
+	
+	private DAO() {}
+	private static DAO obj;
+	
+	public static DAO sharedInstance() {
+		if(obj == null) {
+			obj = new DAO();
+		}
+		return obj;
+	}
+	// 데이터베이스 연동에 필요한 변수들을 선언
+	Connection conn;
+	
+	// SQL 실행에 필요한 변수
+	Statement stmt; 
+	
+	// select 구문을 수행했을 때 결과를 저장할 변수
+	private ResultSet rs;
+	
+	private boolean connect() {
+		boolean result = false;
+		
+		try{
+			conn = DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+			System.out.println("\n- MySQL Connection");
+			result = true;
+		} catch (Exception e) {
+			System.out.println("연결 실패 : " +e.getMessage());
+		}
+		return result;
+	}
+	
+	private void close() {
+		try {
+			if(rs != null)
+				rs.close();
+			if(stmt != null)
+				stmt.close();
+			if(conn != null)
+				conn.close();
+		} catch( Exception e) {
+			System.out.println("해제 실패 : " + e.getMessage());
+		}
+	}
+	
+	public List<User> getUserList() { // select
+		List<User> list = null;
+		String sql = "SELECT * FROM user";
+		if(connect()) {
+			try {
+				stmt = conn.createStatement();
+				if(stmt != null) {
+					rs = stmt.executeQuery(sql);
+					
+					list = new ArrayList<User>();
+					
+					while(rs.next()) {
+						User user = new User();
+						
+//						user.setUserId(rs.getString(rs.getString("userId")));
+//						user.setPwd(rs.getString(rs.getString("pwd")));
+//						user.setName(rs.getString(rs.getString("name")));
+//						user.setBirth(rs.getString(rs.getString("birth")));
+//						user.setAddr(rs.getString(rs.getString("addr")));
+//						user.setPhoneNum(rs.getString(rs.getString("phoneNum")));
+//						user.setPoint(rs.getInt(rs.getString("point")));
+//						user.setTicketPurchaseNum(rs.getInt(rs.getString("ticketPurchaseNum")));
+						
+						user.setUserId((rs.getString("userId")));
+						user.setPwd((rs.getString("pwd")));
+						user.setName((rs.getString("name")));
+						user.setBirth((rs.getString("birth")));
+						user.setAddr((rs.getString("addr")));
+						user.setPhoneNum((rs.getString("phoneNum")));
+						user.setPoint((rs.getInt("point")));
+						user.setTicketPurchaseNum(rs.getInt(("ticketPurchaseNum")));
+						
+						list.add(user);
+					}
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}else {
+			// 연결에 실패했을 때 작업
+			System.out.println("데이터베이스 연결에 실패했습니다.");
+			System.exit(0);
+		}
+		return list;
+	}
+	
+	public boolean InsertUser(User user) {
+		boolean result = false;
+		
+		if(this.connect()) {
+			try {
+				String sql = "INSERT INTO user VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				
+				pstmt.setString(1, user.getUserId());
+				pstmt.setString(2, user.getPwd());
+				pstmt.setNull(3, java.sql.Types.NULL);
+				pstmt.setNull(4, java.sql.Types.NULL);
+				pstmt.setNull(5, java.sql.Types.NULL);
+				pstmt.setNull(6, java.sql.Types.NULL);
+//				pstmt.setString(3, user.getName());
+//				pstmt.setString(4, user.getBirth());
+//				pstmt.setString(5, user.getAddr());
+//				pstmt.setString(6, user.getPhoneNum());
+				pstmt.setNull(7, java.sql.Types.NULL);
+				pstmt.setNull(8, java.sql.Types.NULL);
+				
+				int r = pstmt.executeUpdate();
+				
+				if(r>0) {
+					result = true;
+				}
+				// 데이터베이스 생성 객체 해제
+				pstmt.close();
+				this.close();
+			} catch(SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		} else {
+			System.out.println("데이터베이스 연결에 실패");
+			System.exit(0);
+		}
+		return result;
+	}
+}
--- src/main/SignIn.java
+++ src/main/SignIn.java
@@ -1,11 +1,15 @@
 package main;
 
+import java.util.List;
 import java.util.Scanner;
+
+import Database.DAO;
+import Database.User;
 
 public class SignIn {
 	Scanner scan = new Scanner(System.in);
 	public void run() {
-		String userID = this.inputString("아이디 : ");
+		String userId = this.inputString("아이디 : ");
 		String pwd = this.inputString("비밀번호 : ");
 		String name = this.inputString("성명 (ex. 이성규) : ");
 		String birth = this.inputString("생년월일 (ex. 19940216) : ");
@@ -13,8 +17,30 @@ public class SignIn {
 		String phoneNum = this.inputString("전화번호 : ");
 		
 		// 사용자 테이블에 회원가입 정보가 저장된다 // 회원가입된 정보도 한번 보여줘?
-		System.out.println("회원가입이 완료되었습니다.");
-		// 2 초 정도 타이머?
+		
+		
+		DAO dao = DAO.sharedInstance();
+		User user = new User();
+		user.setUserId(userId);
+		user.setPwd(pwd);
+		user.setName(name);
+		user.setBirth(birth);
+		user.setAddr(addr);
+		user.setPhoneNum(phoneNum);
+		
+//		boolean r = dao.InsertUser(user);
+//		
+//		if(r)
+//			System.out.println("회원가입이 완료되었습니다.");
+//		else
+//			System.out.println("회원가입이 실패하였습니다.");
+
+		// 저장된 거 확인 겸 select 해봄
+		List<User> list = dao.getUserList();
+		
+		for(User u : list) {
+			System.out.println(u);
+		}
 	}
 
 	private int inputInt(String string) {
