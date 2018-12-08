98cbc17d63567778f4ac1a0ed5850340bb9b3563
Seongje KIm <tjdwp3s@gmail.com>
2018-12-08 18:39:09 +0900

    Database test5

--- src/Customer/DeleteUserInfo.java
+++ src/Customer/DeleteUserInfo.java
@@ -2,25 +2,43 @@ package Customer;
 
 import java.util.Scanner;
 
+import Database.DAO;
 import Database.User;
 
 public class DeleteUserInfo {
-	
+
 	Scanner scan = new Scanner(System.in);
-	
+	DAO dao = DAO.sharedInstance();
+
 	public void run(User user) {
-		String sure = this.inputString("정말 탈퇴하시겠습니까? (y / n) ");
+		String sure = this.inputString("정말 탈퇴하시겠습니까? (Y/N) ");
+
+		if (sure.equals("Y")) {
+
+			boolean r = true; // DAO 회원 탈퇴 : 정보 삭제
+
+			if (r) {
+				System.out.println("회원 탈퇴가 완료되었습니다.");
+				System.out.println("프로그램을 종료합니다.");
+				System.exit(0);
+			} else {
+				System.out.println("회원 탈퇴를 실패하였습니다.");
+			}
+
+		} else {
+			System.out.println("회원 탈퇴를 취소합니다.");
+		}
 		// 그냥 바로 삭제
 		// 대신 티켓 등에서의 정보 cascade
-		
+
 		// < 탈퇴하면 프로그램 종료 >
 	}
-	
+
 	private int inputInt(String string) {
 		System.out.print(string);
 		return Integer.parseInt(scan.nextLine());
 	}
-	
+
 	private String inputString(String string) {
 		System.out.print(string);
 		return scan.nextLine();
--- src/Customer/ReserveTicket.java
+++ src/Customer/ReserveTicket.java
@@ -1,52 +1,91 @@
 package Customer;
 
+import java.util.List;
 import java.util.Scanner;
 
+import Database.DAO;
+import Database.Movie;
+import Database.ScreeningMovie;
+import Database.Theater;
 import Database.User;
 
 public class ReserveTicket {
-	
+
 	Scanner scan = new Scanner(System.in);
 	private String movieName;
 	private String theaterName;
 	private String screenDate;
 	private String screenTime;
-	
+
+	DAO dao = DAO.sharedInstance();
+
 	public void run(User user) {
-		// 영화 예매 진행
-		// 영화 제목 리스트를 보여줌
+
+		System.out.println("<<<<<<<<<<<<<<<<<<<< 상영 영화 목록 >>>>>>>>>>>>>>>>>>>>");
+		List<ScreeningMovie> screening_list = dao.getScreeningMovieList(); // DAO 상영 영화 : 상영 영화 목록 가져오기
+		for (ScreeningMovie sm : screening_list) {
+			System.out.println(sm);
+		}
+		System.out.println();
+
+		// 영화 제목
 		movieName = this.inputString("영화 제목을 입력하시오. : ");
-		// 이름이 존재 안함
-		if (movieName.equals("이름이 없네")) {
-			System.out.println("잘 못된 입력입니다.");
-			run(user);
-		}
-		// 영화관 리스트도 보여줌
-		theaterName = this.inputString("영화관을 고르세요. : ");
-		if (theaterName.equals("영화관이 없네")) {
-			System.out.println("잘 못된 입력입니다.");
-			run(user);
-		}
-		// 해당 영화의 영화관의 상영날짜 리스트를 모두 보여줌
-		screenDate = this.inputString("상영날짜를 택하세요.");
-		if (screenDate.equals("상영날짜가 없네")) {
-			System.out.println("잘 못된 입력입니다.");
-			run(user);
-		}
-		// 해당 영화의 영화관의 상영날짜의 상영시간 리스트를 모두 보여줌
-		screenTime = this.inputString("상영시간을 입력하세요. : ");
-		if (screenTime.equals("상영시간이 없네")) {
-			System.out.println("잘 못된 입력입니다.");
-			run(user);
-		}
-		// 앞에 입력받은 정보를 가지고 예약 좌석에서 해당 상여관의 예매좌석과 예약유무를 (좌석수만큼) 모두 출력
-		// <예약이 가능한 좌석 수를 출력>
+		Movie movie = new Movie();
+		movie.setMovieName(movieName);
 		
+		boolean r1 = dao.checkMovieName(movie); // DAO 영화 : 제목 확인
+		
+		if (!r1) {
+			System.out.println("존재하지 않는 영화입니다.");
+			this.run(user);
+		}
+		
+		// 영화관
+		theaterName = this.inputString("영화관을 고르세요. : ");
+		Theater theater = new Theater(theaterName);
+		
+		boolean r2 = dao.checkTheaterId(theater);
+		
+		if (!r2) {
+			System.out.println("존재하지 않는 영화관입니다.");
+			this.run(user);
+		}
+		
+		// 상영날짜
+		boolean r3 = false;
+		screenDate = this.inputString("상영날짜를 택하세요.");
+		for (ScreeningMovie sm : screening_list) {
+			if (sm.getScreenDate() == screenDate) {
+				r3 = true;
+			}
+		}
+		
+		if (!r3) {
+			System.out.println("존재하지 않는 상영날짜입니다.");
+			this.run(user);
+		}
+		
+		// 상영시간
+		boolean r4 = false;
+		screenTime = this.inputString("상영시간을 입력하세요. : ");
+		for (ScreeningMovie sm : screening_list) {
+			if (sm.getScreenTime() == screenTime) {
+				r4 = true;
+			}
+		}
+		
+		if (!r4) {
+			System.out.println("존재하지 않는 상영시간입니다.");
+			this.run(user);
+		}
+		
+		//List<ReserveTicket>
+
 		int howManyReservation = this.inputInt("몇개의 좌석을 예매하겠습니까. : ");
 		// 가능한 좌석 수보다 크다면 ~ howManyReservation을 다시 입력
-		
+
 		// 예약이 안되어 있는 좌석만 예약이가능하도록
-		for(int i=0; i<howManyReservation; i++) {
+		for (int i = 0; i < howManyReservation; i++) {
 			String seatNumber = this.inputString("좌석을 입력하세요 : ");
 			// <if 예약 되지 않은 좌석 중에서 입력과 같은게 없다면 seatNumber다시 입력 받는다
 			// <else 결제 방법 선택> - > 및 예약 1회 완료
@@ -58,15 +97,15 @@ public class ReserveTicket {
 	private void finishReservationWithchoossingPay() {
 		int howToPay = this.inputInt("결제수단을 선택하세요. ( 1. 인터넷 결제 2. 현장 결제 )");
 		switch (howToPay) {
-		case 1 : // 인터넷 결제
+		case 1: // 인터넷 결제
 			this.internetPay();
 			break;
-		case 2 : // 현장 결제
+		case 2: // 현장 결제
 			this.fieldPay();
 			break;
-			default :
-				System.out.println("다시 입력하시오.");
-				this.finishReservationWithchoossingPay();
+		default:
+			System.out.println("다시 입력하시오.");
+			this.finishReservationWithchoossingPay();
 		}
 	}
 
@@ -88,7 +127,7 @@ public class ReserveTicket {
 		System.out.print(string);
 		return Integer.parseInt(scan.nextLine());
 	}
-	
+
 	private String inputString(String string) {
 		System.out.print(string);
 		return scan.nextLine();
