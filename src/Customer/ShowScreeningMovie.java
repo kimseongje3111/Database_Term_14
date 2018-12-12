9619cf187d1fabade1568c809e332877a7db08fc
Seongje KIm <tjdwp3s@gmail.com>
2018-12-12 23:45:10 +0900

    DATABASE_TERM_VER04

--- src/Customer/ShowScreeningMovie.java
+++ src/Customer/ShowScreeningMovie.java
@@ -9,7 +9,6 @@ public class ShowScreeningMovie {
 	DAO dao = DAO.sharedInstance();
 	
 	public void run() {
-		// 현재 상영중인 영화를 예매율순으로 출력
 		
 		List<String> sc_list = dao.printMovieRating(); // DAO 상영 영화 차트 : 예매율 높은 순으로 차트
 
@@ -17,8 +16,7 @@ public class ShowScreeningMovie {
 		int count = 1;
 		for (String s : sc_list) {
 			String[] temp = s.split(",");
-			float reserve_rate = Float.parseFloat(temp[2]) / Float.parseFloat(temp[1]);
-			System.out.println(String.format("[%d] %s / 예매율 : %.2f", count, temp[0], reserve_rate));
+			System.out.println(String.format("[%d] %s / 예매율 : %.2f", count, temp[0], temp[3]));
 			count++;
 		}
 		System.out.println();
